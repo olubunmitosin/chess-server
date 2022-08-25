fb3eaee2f848a93e30014e8e1b3f2009cda88a0b
Olubunmi Tosin <olubunmivictor6@gmail.com>
2022-08-25 21:03:55 +0100

    Fixes

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>COBS Chess Server</title>
+</head>
+<body>
+<div style="background-color: #f5f5f5; padding: 10px;">
+    <div style="text-align: center; padding-top: 200px; color: dimgray;">
+        You're in the wrong place!
+    </div>
+</div>
+</body>
+</html>
